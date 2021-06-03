2b53d00092134fc02b2ff1aea2952c0e51495e63
d4rk-kn1gh7 <anandabalaji@gmail.com>
2021-06-03 15:46:18 +0530

    DNS lookup works

--- client.py
+++ client.py
@@ -12,9 +12,11 @@ class NetworkCapture():
 
     ethproto = {0x0806: 'ARP', 0x0800: 'IPv4', 0x86dd: 'IPv6'}
     netproto = {1: "ICMP", 17: "UDP", 6: "TCP"}
+    website_maps = dict()
 
-    def __init__(self):
+    def __init__(self, websites):
         self.finalData = dict()
+        self.parse_websites(websites)
 
     def getTCPdata(self, data):
         proto = pwn.u16(data[12:14], endian = 'big')
@@ -51,9 +53,15 @@ class NetworkCapture():
                 ) and len(data) > 0:
                     logging.info("HTTP!!!")
                     if dest_port == 80 or dest_port == 443:
-                        website = self.dns_lookup(dest_ip)
+                        try:
+                            website = self.website_maps[dest_ip]
+                        except:
+                            website = self.dns_lookup(dest_ip)
                     else:
-                        website = self.dns_lookup(src_ip)
+                        try:
+                            website = self.website_maps[src_ip]
+                        except:
+                            website = self.dns_lookup(src_ip)
                     if "." in website:
                         logging.info(f'Protocol: {self.netproto[proto]}, Source IP: {src_ip}, Destination IP: {dest_ip}')
                         logging.info(f'Source Port: {src_port}, Destination Port: {dest_port}')
@@ -66,16 +74,23 @@ class NetworkCapture():
         return self.finalData
         
     def dns_lookup(self, ip):
-        data = os.popen(f"dig +noall +answer -x {ip}", "r").read()
-        return data.split(" ")[-1].split("\t")[-1].replace("\n", "")
+        data = os.popen(f"dig -x {ip} +short", "r").read()
+        return data.strip().strip(".")
+    
+    def parse_websites(self, websites):
+        with open(websites, "r") as f:
+            for line in f.readlines():
+                data = line.strip("\n")
+                self.website_maps[os.popen(f"dig {data} +short", "r").read().strip("\n")] = data
 
 class Client():
 
-    def __init__(self, server, port, device, interface):
+    def __init__(self, server, port, device, interface, websites):
         self.server = server
         self.port = int(port)
         self.device = device
         self.interface = interface
+        self.websites = websites
 
     def exec(self):
         sock = socket.socket(socket.PF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0003))
@@ -86,7 +101,7 @@ class Client():
     def capture(self, sock):
         while True:
             data = sock.recv(65565)
-            net_cap = NetworkCapture()
+            net_cap = NetworkCapture(self.websites)
             output = net_cap.getTCPdata(data)
             self.send2server(output)
             
@@ -109,13 +124,16 @@ if __name__ == "__main__":
     parser.add_argument("-p", "--port", help = "Server port", required = False, default = 8000)
     parser.add_argument("-i", "--interface", help = "Interface", required = False, default = None)
     parser.add_argument("-d", "--device", help = "Device name", required = False, default = os.uname()[1])
+    parser.add_argument("-w", "--websites", help = "Website file", required = False, default = "websites.txt")
 
     args = parser.parse_args()
 
     logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
 
     try:
-        c = Client(server = args.server, port = args.port, device = args.device, interface = args.interface)
+        c = Client(
+            server = args.server, port = args.port, device = args.device, interface = args.interface, websites = args.websites
+        )
         c.exec()
 
     except Exception as e:
